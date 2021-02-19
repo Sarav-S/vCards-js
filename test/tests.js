32da6996b4bce3bcd96edb0ddea759b7c2217a1d
venki0004 <74340982+venki0004@users.noreply.github.com>
2021-02-19 15:19:05 +0530

    Update tests.js

--- test/tests.js
+++ test/tests.js
@@ -76,7 +76,14 @@ describe('vCard', function() {
     testCard.workAddress.stateProvince = 'CA';
     testCard.workAddress.postalCode = '54321';
     testCard.workAddress.countryRegion = 'California Republic';
-
+    
+    
+    testCard.otherAddress.label = 'Other Address';
+    testCard.otherAddress.street = '123 Corporate Loop\nSuite 500';
+    testCard.otherAddress.city = 'Los Angeles';
+    testCard.otherAddress.stateProvince = 'CA';
+    testCard.otherAddress.postalCode = '54321';
+    testCard.otherAddress.countryRegion = 'California Republic';
     testCard.source = 'http://sourceurl';
     testCard.note = 'John Doe\'s \nnotes;,';
 
